9a2c63f3b53e9bbc4bb1fbc868326f6f18945da2
Zayebalo <egor3942@mail.ru>
2025-03-04 15:18:34 +0300

    Хпбар, кровать, таймер, спавн модов, очки

---
+++ Assets/Scripts/Other/GameInput.cs
@@ -0,0 +1,51 @@
+using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class GameInput : MonoBehaviour
+{
+    public static GameInput Instance { get; private set; }
+
+    private PlayerInputAction _playerInputActions;
+
+    public event EventHandler OnPlayerAttack;
+    public event EventHandler OnPlayerInteract;
+
+    private void Awake()
+    {
+        Instance = this;
+        _playerInputActions = new PlayerInputAction();
+        _playerInputActions.Enable();
+
+        _playerInputActions.Combat.Attack.started += PlayerAttack_started;
+        _playerInputActions.Interaction.Interaction.started += Interaction_started;
+    }
+
+    
+
+    public Vector2 GetMovementVector()
+    {
+        Vector2 inputVector = _playerInputActions.Player.Move.ReadValue<Vector2>();
+        return inputVector;
+    }
+
+    public Vector3 GetMousePosition()
+    {
+        Vector3 mousePos = Mouse.current.position.ReadValue();
+        return mousePos;
+    }
+
+    public void DisableMovement()
+    {
+        _playerInputActions.Disable();
+    }
+
+    private void PlayerAttack_started(InputAction.CallbackContext obj)
+    {
+        OnPlayerAttack?.Invoke(this, EventArgs.Empty); // if (OnPlayerAttack != null) но короче
+    }
+    private void Interaction_started(InputAction.CallbackContext obj)
+    {
+        OnPlayerInteract?.Invoke(this, EventArgs.Empty);
+    }
+}
